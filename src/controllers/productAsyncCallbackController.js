e28af2472bee07f527e86fd79b1c5c2b826e4eb9
Bruno Caramelo <bruno.caramelo5@gmail.com>
2025-08-22 08:06:13 -0300

    max rety

--- src/controllers/productAsyncCallbackController.js
+++ src/controllers/productAsyncCallbackController.js
@@ -15,28 +15,33 @@ exports.productByUrlAsyncCallback = async (req, res) => {
       res.status(200).json({ status: 'success', message: 'Processamento iniciado' });
 
       setImmediate(async () => {
-          try {
-            console.log(`[BG] Iniciando scraping para ${targetUrl}`);
+          const maxRety = 4;
 
-            // const targetUrlProduct = Buffer.from(targetUrl, 'utf-8').toString('base64');
-            const targetUrlProduct = targetUrl;
+          for (let retryCount = 1; retryCount <= 4; retryCount++) {
+            try {
+              console.log(`[BG] Iniciando scraping para ${targetUrl}`);
 
-            const getProductData = await new ScrapperProduct(targetUrlProduct).handle();
+              const getProductData = await new ScrapperProduct(targetUrl).handle();
 
-            console.log(`[BG] Scraping concluído para ${externalId}`);
+              console.log(`[BG] Scraping concluído para ${externalId}`);
 
-            await fetch(callbackUrl, {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({
-                external_id: externalId,
-                data: getProductData
-              })
-            });
+              await fetch(callbackUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                  external_id: externalId,
+                  data: getProductData
+                })
+              });
 
-            console.log(`[BG] Callback enviado para ${callbackUrl}`);
-          } catch (err) {
-            console.error(`[BG] Erro no processamento (${externalId}):`, err);
+              console.log(`[BG] Callback enviado para ${callbackUrl}`);
+            } catch (err) {
+              console.error(`[BG] Erro no processamento (${externalId}):`, err);
+              if (retryCount === MAX_RETRIES) {
+                  console.error(`[BG] Todas as ${maxRety} tentativas falharam para ${externalId}. Lançando exceção.`);
+                  throw new Error(`Failed to process and callback after ${maxRety} retries for ${externalId} to get data into: ${callbackUrl}`);
+              }
+            }
           }
         });
 
